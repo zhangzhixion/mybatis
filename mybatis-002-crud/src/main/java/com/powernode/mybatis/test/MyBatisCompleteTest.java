1ac7baaf6c794cdf5deec8f9e0daa41066fad77d
zzx <zzx@xiong.com>
2023-11-23 14:04:24 +0800

    hot-fix commit1

--- mybatis-002-crud/src/main/java/com/powernode/mybatis/test/MyBatisCompleteTest.java
+++ mybatis-002-crud/src/main/java/com/powernode/mybatis/test/MyBatisCompleteTest.java
@@ -36,6 +36,7 @@ public class MyBatisCompleteTest {
             }
             System.out.println("hello git");
             System.out.println("hello github");
+            System.out.println("hello hot-fix");
         }
     }
 }
