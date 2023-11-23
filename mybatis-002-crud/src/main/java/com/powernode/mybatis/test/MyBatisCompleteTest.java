74a35e50874c92b4b99c3054f03e59eefb4f7804
zhangzhixion <151739939+zhangzhixion@users.noreply.github.com>
2023-11-23 19:00:39 +0800

    pull commit

--- mybatis-002-crud/src/main/java/com/powernode/mybatis/test/MyBatisCompleteTest.java
+++ mybatis-002-crud/src/main/java/com/powernode/mybatis/test/MyBatisCompleteTest.java
@@ -40,6 +40,7 @@ public class MyBatisCompleteTest {
             System.out.println("hello master");
             System.out.println("hello hot-fix2");
             System.out.println("push mybatis");
+            System.out.println("pull mybatis");
         }
     }
 }
